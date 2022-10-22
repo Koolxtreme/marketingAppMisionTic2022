12d7cb3e1ed01abfaecb9455caae16aae181b531
Koolxtreme <koolxtreme@gmail.com>
2022-10-22 15:18:29 -0500

    Se encuentra y corrige fallo en el helper de la consulta principal

--- server/helpers/functions.js
+++ server/helpers/functions.js
@@ -1,22 +1,28 @@
 export const obtenerMejores = (a, ismain) => {
-    let irterable = a.map((objeto) => objeto);
-    irterable.sort((n1, n2) => n2.pop - n1.pop);
-    const result = [];
-    if (ismain) {
+  let irterable = a.map((objeto) => objeto);
+  irterable.sort((n1, n2) => n2.pop - n1.pop);
+  const result = [];
+  if (ismain) {
+    if (irterable.length > 7) {
       for (let i = 0; result.length < 7; i++) {
         result.push(irterable[i]);
       }
     } else {
-      if (irterable.length > 20) {
-        for (let i = 0; result.length < 20; i++) {
-          result.push(irterable[i]);
-        }
-      } else {
-        for (let i = 0; result.length < irterable.length; i++) {
-          result.push(irterable[i]);
-        }
+      for (let i = 0; result.length < irterable.length; i++) {
+        result.push(irterable[i]);
       }
     }
-  
-    return result;
-  };
+  } else {
+    if (irterable.length > 20) {
+      for (let i = 0; result.length < 20; i++) {
+        result.push(irterable[i]);
+      }
+    } else {
+      for (let i = 0; result.length < irterable.length; i++) {
+        result.push(irterable[i]);
+      }
+    }
+  }
+
+  return result;
+};
